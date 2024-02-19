fc458f3f8707a24a6c3763eb2adc92ce247e7794
Plutzz <benjiongco@gmail.com>
2024-02-19 15:50:47 -0800

    Create Custom Input Manager

---
+++ Assets/_Project/_Scripts/Input Manager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Custom input handler class to be able to have rebindable inputs
+public class InputManager : Singleton<InputManager>
+{
+    // Move Input
+    public Vector2 MoveInput {  get; private set; }
+    // Interact input
+    public bool InteractPressedThisFrame {  get; private set; }
+    public bool InteractReleasedThisFrame { get; private set; }
+    public bool InteractIsPressed { get; private set; }
+
+
+    private PlayerInput playerInput;
+
+    private InputAction moveAction;
+    private InputAction interactAction;
+
+    protected override void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        SetupInputActions();
+    }
+
+    private void Update()
+    {
+        UpdateInputs();
+    }
+
+    private void SetupInputActions()
+    {
+        moveAction = playerInput.actions["Move"];
+        interactAction = playerInput.actions["Interact"];
+    }
+
+    private void UpdateInputs()
+    {
+        // Move Input Variables
+        MoveInput = moveAction.ReadValue<Vector2>();
+
+        // Interact Input Variables
+        InteractPressedThisFrame = interactAction.WasPressedThisFrame();
+        InteractIsPressed = interactAction.IsPressed();
+        InteractReleasedThisFrame = interactAction.WasReleasedThisFrame();
+    }
+
+
+
+
+}
