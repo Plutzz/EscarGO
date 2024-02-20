850aa69a0a64a18e1e2971994cdaec952555647a
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-02-19 23:07:43 -0800

    start of minigame

---
+++ Assets/_Project/_Scripts/Knife.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : Interactable
+{
+    private bool success = false;
+    public override void Activate()
+    {
+        
+    }
+
+    public override void DeActivate()
+    {
+
+    }
+}
