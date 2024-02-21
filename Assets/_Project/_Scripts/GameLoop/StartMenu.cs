3ddc66e4bad9eaf4fa4650f172f7a326bd45c6b3
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-02-20 17:19:24 -0800

    created new scenes

---
+++ Assets/_Project/_Scripts/GameLoop/StartMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject startMenu;
+    [SerializeField] private GameObject settingMenu;
+
+    public void GoLobby()
+    {
+        // Go Lobby here
+    }
+}
