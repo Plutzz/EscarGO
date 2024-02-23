bc741e5b5c8d5fbd850732295d25d6fa6c003e82
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-02-22 19:16:15 -0800

    Creating cutting minigame

--- Assets/_Project/_Scripts/CuttingStation.cs
+++ Assets/_Project/_Scripts/CuttingStation.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class CuttingStation : SuperStation
 {
+    [SerializeField] private GameObject cutIndicator;
+    private LayerMask minigameLayer;
     private bool success = false;
+    private Ray ray;
+
+
     public override void Activate()
     {
+        //Instantiate
     }
 
     public override void DeActivate()
@@ -17,5 +23,41 @@ public class CuttingStation : SuperStation
     {
         get { return success; }
         set { success = value; }
+    }
+
+    private void Start()
+    {
+        minigameLayer = LayerMask.GetMask("Minigame");
+    }
+
+    private void Update() {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(CheckHit())
+            {
+
+            }
+        }
+
+        if (Physics.Raycast(ray, out RaycastHit playerStart, 900f, minigameLayer))
+        {
+            
+        }
+    }
+
+    private bool CheckHit()
+    {
+        if (Physics.Raycast(ray, out RaycastHit playerStart, 900f, minigameLayer))
+        {
+            return true;   
+        } else {
+            return false;
+        }
+    }
+
+    private void MoveNext()
+    {
     }
 }
