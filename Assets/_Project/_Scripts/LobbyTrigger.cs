69e584259255490212770c302bcf4be9273d382f
Plutzz <benjiongco@gmail.com>
2024-03-10 11:40:13 -0700

    Improve Lobby Trigger

--- Assets/_Project/_Scripts/LobbyTrigger.cs
+++ Assets/_Project/_Scripts/LobbyTrigger.cs
@@ -1,36 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class LobbyTrigger : NetworkBehaviour
 {
     [SerializeField] private string SceneName;
-    private int numPlayersInCollider = 0;
+    [SerializeField] private TextMeshPro LobbyText;
+    private int numPlayersReady = 0;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!IsServer) return;
-
         if (collision.gameObject.CompareTag("Player"))
         {
-            numPlayersInCollider++;
-
-            if (numPlayersInCollider >= 1)
-            {
-                Debug.Log("Loading Next Scene");
-                NetworkManager.Singleton.SceneManager.LoadScene(SceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
-            }
+            PlayerReadyServerRpc();
         }
     }
+    [ServerRpc(RequireOwnership = false)]
+    private void PlayerReadyServerRpc()
+    {
+        numPlayersReady++;
+        UpdateLobbyTextClientRpc(numPlayersReady, NetworkManager.Singleton.ConnectedClientsList.Count);
+
+        if (numPlayersReady == NetworkManager.Singleton.ConnectedClientsList.Count)
+        {
+            Debug.Log("Loading Next Scene");
+            NetworkManager.Singleton.SceneManager.LoadScene(SceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+    }
+
     private void OnCollisionExit(Collision collision)
     {
-        if (!IsServer) return;
-
         if (collision.gameObject.CompareTag("Player"))
         {
-            numPlayersInCollider -= 1;
+            PlayerUnreadyServerRpc();
         }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void PlayerUnreadyServerRpc()
+    {
+        numPlayersReady--;
+        UpdateLobbyTextClientRpc(numPlayersReady ,NetworkManager.Singleton.ConnectedClientsList.Count);
+    }
+    [ClientRpc]
+    private void UpdateLobbyTextClientRpc(int _playersReady, int _playersConnected)
+    {
+        LobbyText.text = _playersReady + " / " + _playersConnected + " Players Ready";
     }
 
 
