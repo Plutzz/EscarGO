fa1a0acd98dabdb195042f5ecf7dcb41d998c42d
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-03-14 21:04:00 -0700

    battershaping script

--- Assets/BatterShapingStation.cs
+++ Assets/BatterShapingStation.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using Unity.Netcode;
 
-public class BatterShapingStation : MonoBehaviour
+public class BatterShapingStation : SuperStation
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private CraftableItem batter;
+    private PlayerInventory inventory;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private GameObject batterCircle;
+    private bool success = false;
+
+    private bool isBattering = false;
+    private bool squeezing = false;
+
+    public override void Activate()
     {
-        
+        inventory = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<PlayerInventory>();
+        virtualCamera.enabled = true;
+        isBattering = true;
+
+        NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<InputManager>().playerInput.SwitchCurrentActionMap("MiniGames");
+        Cursor.lockState = CursorLockMode.None;
+    }
+    
+    public override void DeActivate()
+    {
+        squeezing = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<InputManager>().playerInput.SwitchCurrentActionMap("Player");
+        virtualCamera.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool ActivityResult
     {
-        
+        get { return success; }
+        set { success = value; }
+    }
+
+    public override CinemachineVirtualCamera VirtualCamera
+    {
+        get { return virtualCamera; }
+        set { virtualCamera = value; }
+    }
+
+    private void Update() {
+        if(isBattering)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                GameObject batter = Instantiate(batterCircle, transform.position + new Vector3(0, 0.52f, 0.018f), transform.rotation);
+                squeezing = true;
+            }
+        }
     }
 }
