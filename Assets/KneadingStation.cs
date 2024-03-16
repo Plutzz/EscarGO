009ba7751a594751db5010d3f667c48ce4f90b13
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-03-16 01:17:56 -0700

    kneading station script fnished

--- Assets/KneadingStation.cs
+++ Assets/KneadingStation.cs
@@ -7,14 +7,26 @@ using Unity.Netcode;
 
 public class KneadingStation : SuperStation
 {
-    [SerializeField] private CraftableItem batter;
+    [SerializeField] private float goalSizeOfDough = 1f;
+    [SerializeField] private GameObject squareOfDough;
+    private Vector3 doughOffset = new Vector3(0, 0.52f, 0.018f);
+
+    [SerializeField] private CraftableItem kneadedDough;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
     private PlayerInventory inventory;
     private bool success;
 
+    private GameObject playerDough;
     private bool isKneading = false;
+    private int[] keySequence;
+    private int wantedKey = 0;
+    private bool noFirstKey = true;
 
     public override void Activate()
     {
+        playerDough = Instantiate(squareOfDough, transform.position + doughOffset, transform.rotation);
+        isKneading = true;
+
         inventory = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<PlayerInventory>();
         virtualCamera.enabled = true;
         isKneading = true;
@@ -25,6 +37,9 @@ public class KneadingStation : SuperStation
     public override void DeActivate()
     {
         isKneading = false;
+        noFirstKey = true;
+        wantedKey = 0;
+        Destroy(playerDough);
 
         NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<InputManager>().playerInput.SwitchCurrentActionMap("Player");
         virtualCamera.enabled = false;
@@ -42,15 +57,101 @@ public class KneadingStation : SuperStation
         set { virtualCamera = value; }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(isKneading)
+        {
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                if(noFirstKey)
+                {
+                    noFirstKey = false;
+                    wantedKey = 2;
+                }
+
+                if(PressedSequence(2))
+                {
+                    playerDough.transform.localScale += new Vector3(0, 0, 0.05f);
+                }
+
+            } else if(Input.GetKeyDown(KeyCode.A))
+            {
+                if(noFirstKey)
+                {
+                    noFirstKey = false;
+                    wantedKey = 1;
+                }
+
+                if(PressedSequence(1))
+                {
+                    playerDough.transform.localScale += new Vector3(0.05f, 0, 0);
+                }
+
+            } else if(Input.GetKeyDown(KeyCode.S))
+            {
+                if(noFirstKey)
+                {
+                    noFirstKey = false;
+                    wantedKey = 4;
+                }
+
+                if(PressedSequence(4))
+                {
+                    playerDough.transform.localScale += new Vector3(0, 0, 0.05f);
+                }
+
+            } else if(Input.GetKeyDown(KeyCode.D))
+            {
+                if(noFirstKey)
+                {
+                    noFirstKey = false;
+                    wantedKey = 3;
+                }
+
+                if(PressedSequence(3))
+                {
+                    playerDough.transform.localScale += new Vector3(0.05f, 0, 0);
+                }
+
+            }
+
+            if(playerDough.transform.localScale.x >= goalSizeOfDough && playerDough.transform.localScale.z >= goalSizeOfDough)
+            {
+                Succeed();
+            }
+        }
+    }
+
+    private bool PressedSequence(int pressed)
+    {
+        Debug.Log("pressed = " + pressed + " | " + "wantedKey = " + wantedKey);
+
+        if(pressed == wantedKey)
+        {
+            Debug.Log("correct");
+
+            wantedKey += 1;
+            if(wantedKey > 4)
+            {
+                wantedKey = 1;
+            }
+
+            return true;
+        }
+
+        Debug.Log("wrong");
+        return false;
+    }
+
+    private void Succeed()
+    {
+        success = true;
+        if(inventory.CanCraft(kneadedDough))
+        {
+            inventory.Craft(kneadedDough);
+        }
+
+        DeActivate();
     }
 }
