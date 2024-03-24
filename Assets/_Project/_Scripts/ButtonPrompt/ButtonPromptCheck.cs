ff56815f79d25006c5d92ff13dfc74d98402c3f7
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-03-24 14:17:30 -0700

    added fix

--- Assets/_Project/_Scripts/ButtonPrompt/ButtonPromptCheck.cs
+++ Assets/_Project/_Scripts/ButtonPrompt/ButtonPromptCheck.cs
@@ -69,6 +69,10 @@ public class ButtonPromptCheck : MonoBehaviour
                 }
                 
             }
+            else
+            {
+                ClearUIItem();
+            }
         }
 
         if (!foundButtonPrompt)
