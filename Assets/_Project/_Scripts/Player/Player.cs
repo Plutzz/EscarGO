dcb71247a97ca8de316c0a92884a056e69c70bda
Plutzz <benjiongco@gmail.com>
2024-04-14 17:51:29 -0700

    Remove jumping and clean up player script

--- Assets/_Project/_Scripts/Player/Player.cs
+++ Assets/_Project/_Scripts/Player/Player.cs
@@ -22,30 +22,14 @@ public class Player : NetworkBehaviour
     private PlayerState currentState;
     private Rigidbody rb;
     private InputManager inputManager;
-    #endregion
 
-    # region Stamina Variables
-    [Header("Stamina Variables")]
-    [SerializeField] private float maxStamina = 100f;
-    public float currentStamina;
-    public float staminaDecreaseRate = 10f;
-    public float staminaIncreaseRate = 5f;
+    [Header("Movement Variables")]
     [SerializeField] private float sprintSpeed = 5f;
     private float moveSpeed;
     private bool sprinting = false;
     public bool canSprint = true;
     #endregion
 
-    # region Jumping Variables
-    [Header("Jumping Variables")]
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float jumpCooldownDuration = 1f;
-    [SerializeField] private float jumpBufferTime = 0.1f;
-    private float jumpCooldown;
-    public bool canJump = true;
-    private float lastJumpPressed;
-
-    #endregion
     [SerializeField] private SettingsMenu pauseMenu;
     [SerializeField] private FirstPersonCamera cameraScript;
     [SerializeField] private Vector3 spawnPos;
@@ -71,8 +55,6 @@ public class Player : NetworkBehaviour
         stateMachine = GetComponent<PlayerStateMachine>();
         moveSpeed = stateMachine.moveSpeed;
         rb = stateMachine.rb;
-        currentStamina = maxStamina;
-        canJump = true;
         inputManager = GetComponent<InputManager>();
     }
 
@@ -97,32 +79,16 @@ public class Player : NetworkBehaviour
 
         currentState = stateMachine.currentState;
 
-        if (jumpCooldown <= Time.time)
-        {
-            canJump = true;
-        }
-
-        if (stateMachine.inputManager.JumpPressedThisFrame)
-        {
-            lastJumpPressed = Time.time;
-            if (currentState == stateMachine.AirborneState) return;
-
-            if (canJump && lastJumpPressed + jumpBufferTime >= Time.time)
-            {
-                GroundedJump();
-            }
-        }
-
         if (currentState == stateMachine.AirborneState) return;
         
 
         if (sprinting && canSprint)
         {
-            DecreaseStamina();
+            stateMachine.moveSpeed = sprintSpeed;
         }
         else
         {
-            IncreaseStamina();
+            stateMachine.moveSpeed = moveSpeed;
         }
 
         if(inputManager.PausePressedThisFrame && pauseMenu != null)
@@ -133,52 +99,5 @@ public class Player : NetworkBehaviour
         if(orientation != null && cameraScript != null)
             orientation.eulerAngles = new Vector3 (0f, cameraScript.transform.eulerAngles.y, 0f);
 
-    }
-
-
-    private void DecreaseStamina()
-    {
-        currentStamina -= staminaDecreaseRate * Time.deltaTime;
-        stateMachine.moveSpeed = sprintSpeed;
-        currentStamina = Mathf.Max(currentStamina, 0);
-        if (currentStamina <= 0)
-        {
-            canSprint = false;
-        }
-    }
-
-    private void IncreaseStamina()
-    {
-        currentStamina += staminaIncreaseRate * Time.deltaTime;
-        stateMachine.moveSpeed = moveSpeed;
-        currentStamina = Mathf.Min(currentStamina, maxStamina);
-        if (currentStamina < maxStamina * 0.5f && currentStamina > 0)
-            canSprint = false;
-        else
-            canSprint = true;
-
-    }
-
-    private void GroundedJump()
-    {
-        canJump = false;
-        jumpCooldown = Time.time + jumpCooldownDuration;
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-
-    [ContextMenu("Spectate")]
-    public void Spectate(bool isSpectating) {
-        stateMachine.cameraTransform.localPosition = localCameraPosition;
-
-        foreach(GameObject gameObject in toggledGameObjects){ 
-            gameObject.SetActive(!isSpectating);
-        }
-
-        foreach (MonoBehaviour thing in toggledMonoBehaviours) { 
-            thing.enabled = !isSpectating;
-        }
-
-    
     }
 }
