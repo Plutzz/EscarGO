2ba6ee287758f69f7011c86d5a2a1372f45a92e7
Plutzz <benjiongco@gmail.com>
2024-04-30 17:53:23 -0700

    Change cart spawning

--- Assets/CartManager.cs
+++ Assets/CartManager.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 
 
 // Spawns a set amount of carts into the scene
-public class CartManager : NetworkBehaviour
+public class CartManager : NetworkSingleton<CartManager>
 {
     public int amountOfCarts = 5;
     public GameObject cartPrefab;
     public Transform spawnPoint;
-    public float cartInterval = 15f;
     public float cartSpeed = 10f;
+    public float cartsAlive = 0;
 
     public override void OnNetworkSpawn()
     {
         if(!IsServer) return;
-        //StartCoroutine(SpawnCarts());
     }
     void Update()
     {
         if (!IsServer) return;
-        cartInterval -= Time.deltaTime;
-        if (cartInterval <= 0)
+        if (cartsAlive <= 0)
         {
+            cartsAlive = 0;
             StartCoroutine(SpawnCarts());
-            cartInterval = 30f;
         }
     }
 
@@ -33,11 +31,13 @@ public class CartManager : NetworkBehaviour
     {
         for (int i = 0; i < amountOfCarts; i++)
         {
+            cartsAlive++;
             GameObject cart = Instantiate(cartPrefab, spawnPoint.position, Quaternion.identity);
             Rigidbody rb = cart.GetComponentInChildren<Rigidbody>();
             rb.velocity = new Vector3(-cartSpeed, 0, 0);
             rb.angularVelocity = new Vector3(0, 1, 0);
             cart.GetComponent<NetworkObject>().Spawn(true);
+
             yield return new WaitForSeconds(1);
         }
     }
